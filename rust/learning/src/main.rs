ad1399641a3a87a26fe9a9702d27996202a89b9e
clement3872 <clement.noisy78@gmail.com>
2023-09-19 01:02:18 +0200

    Update rust/learning

--- rust/learning/src/main.rs
+++ rust/learning/src/main.rs
@@ -1,8 +1,70 @@
-//use std::io;
+use std::io;
 use rand::Rng;
 
-fn main() {
-    let rng_num = rand::thread_rng().gen_range(0..=1);
 
-    println!("{}", rng_num);
+fn random_number(n0: i32, n1:i32) -> i32{
+    return rand::thread_rng().gen_range(n0..=n1);
 }
+
+
+
+fn guess_the_number(){
+    let mut l_numbers: Vec<String> = Vec::new();
+    for i in 0..10 {
+        l_numbers.push(i.to_string());
+    }
+    l_numbers.push(String::from("-"));
+    l_numbers.push(String::from(""));
+
+    let to_guess = random_number(1,100) as u8;
+    println!("number to guess: {}", to_guess);
+
+    println!("Enter a number between 0 and 100: ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let _ = input.trim();
+
+    // something wrong here ?
+    let mut can_be_int = false;
+    while !can_be_int{
+        can_be_int = true;
+        for c in input.chars(){
+            if !l_numbers.contains(&String::from(c)){
+                can_be_int = false;
+                println!("pates");
+            }
+        }
+        if !can_be_int{
+            println!("Don't you know what a number is? Try again.");
+            io::stdin().read_line(&mut input).expect("Failed to read line");
+            let _ = input.trim();
+        }
+    }
+
+    let input: u8 = input.trim().parse().unwrap();
+    
+}
+
+fn main() {
+    guess_the_number();
+
+
+    /*
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input);
+    
+    let my_num: i32 = input.trim().parse()
+        .expect("please give me a number!");
+
+    match input.trim().parse::<i32>(){
+        Ok(n) => n,
+        Err(_) => None,
+    };
+
+    println!("{}", my_num*2);
+    */ 
+}
+
