f93ca7c992f4f4a3dad33c67ab34e484541bb0ac
clement3872 <clement.noisy78@gmail.com>
2023-09-19 15:22:11 +0200

    update rust learning

--- rust/learning/src/main.rs
+++ rust/learning/src/main.rs
@@ -9,12 +9,9 @@ fn random_number(n0: i32, n1:i32) -> i32{
 
 
 fn guess_the_number(){
-    let mut l_numbers: Vec<String> = Vec::new();
-    for i in 0..10 {
-        l_numbers.push(i.to_string());
-    }
-    l_numbers.push(String::from("-"));
-    l_numbers.push(String::from(""));
+    let l_numbers: String = String::from("-1234567890");
+    let l_numbers = l_numbers.chars().collect::<Vec<_>>();
+
 
     let to_guess = random_number(1,100) as u8;
     println!("number to guess: {}", to_guess);
@@ -24,47 +21,66 @@ fn guess_the_number(){
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    let _ = input.trim();
+    let _ = input.trim().replace("\n","");
 
-    // something wrong here ?
+
     let mut can_be_int = false;
     while !can_be_int{
         can_be_int = true;
         for c in input.chars(){
-            if !l_numbers.contains(&String::from(c)){
+            if !l_numbers.contains(&c){
                 can_be_int = false;
-                println!("pates");
             }
         }
         if !can_be_int{
             println!("Don't you know what a number is? Try again.");
+            input = String::from("");
             io::stdin().read_line(&mut input).expect("Failed to read line");
             let _ = input.trim();
         }
     }
 
-    let input: u8 = input.trim().parse().unwrap();
-    
+    let mut input_int: u8 = input.trim().parse().unwrap();
+
+    while input_int != to_guess{
+        println!("Your input: {}", input_int);
+        if to_guess > input_int{println!("It's more");}
+        else{println!("It's less");}
+
+        input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let _ = input.trim();
+        
+        can_be_int = false;
+        while !can_be_int{
+            can_be_int = true;
+            println!("{}", input.len());
+            if input.len() > 0{
+                for c in input.chars(){
+                    if !l_numbers.contains(&c){
+                        can_be_int = false;
+                    }
+                }
+            } else {
+                println!("COUCOU");
+                can_be_int = false;}
+            if !can_be_int {
+                println!("Don't you know what a number is? Try again.");
+                input = String::from("");
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                let _ = input.trim();
+            }
+        }
+        input_int = input.trim().parse().unwrap();
+    }
+
+    println!("GG! you guessed my number!");
 }
+
 
 fn main() {
     guess_the_number();
-
-
-    /*
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input);
-    
-    let my_num: i32 = input.trim().parse()
-        .expect("please give me a number!");
-
-    match input.trim().parse::<i32>(){
-        Ok(n) => n,
-        Err(_) => None,
-    };
-
-    println!("{}", my_num*2);
-    */ 
 }
 
