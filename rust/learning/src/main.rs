46a17aeb15977704e80e2c085932f45ed5ff1a99
clement3872 <clement.noisy78@gmail.com>
2023-10-02 00:03:32 +0200

    update rust/learning

--- rust/learning/src/main.rs
+++ rust/learning/src/main.rs
@@ -1,7 +1,7 @@
 
 fn examples_for_lists() {
     // init a list - here are some different ways
-    let v: Vec<i32> = Vec::new(); 
+    let v: Vec<i32> = Vec::new();
     println!("{:?}", v);
     let mut v = vec![1,2,3];
 
@@ -12,8 +12,33 @@ fn examples_for_lists() {
 }
 
 
-fn main(){
-    println!("Some example of lists:");
-    examples_for_lists(); 
+fn fibo(n: u32) -> u128 {
+    let mut a: u128 = 0;
+    let mut b: u128 = 1;
+    let mut tmp: u128;
+
+    if n > 0 {
+        for _ in 0..(n - 1) {
+            tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+    } else {
+        b = 0;
+    }
+
+    b // clippy likes this way of returns, I don't
 }
 
+fn main() {
+    examples_for_lists();
+    println!(""); // just some space
+    for i in 0..10 {
+        println!("fibo({}) = {}", i, fibo(i));
+    }
+}
+
+#[test]
+fn test_fibo() {
+    assert_eq!(fibo(186), 332825110087067562321196029789634457848);
+}
